328c7b128878937d14aefafba762b2c8d5b95ed7
Hoangnam-04 <xekenvin123@gmail.com>
2026-09-17 11:49:35 +0700

    Update Apply and JobPost classes

--- SWP391/src/java/model/ApplyModel.java
+++ SWP391/src/java/model/ApplyModel.java
@@ -12,6 +12,14 @@ public class ApplyModel {
     private String candidatePhone;
     private int cvId;
     private String cvPosition;
+    private Integer cvNumberExp;
+    private String cvEducation;
+    private String cvField;
+    private java.math.BigDecimal cvCurrentSalary;
+    private java.sql.Date cvBirthday;
+    private String cvNationality;
+    private String cvGender;
+    private String cvFileData;
     private String status;
     private String note;
     private String finalResult;
@@ -48,6 +56,30 @@ public class ApplyModel {
 
     public String getCvPosition() { return cvPosition; }
     public void setCvPosition(String cvPosition) { this.cvPosition = cvPosition; }
+
+    public Integer getCvNumberExp() { return cvNumberExp; }
+    public void setCvNumberExp(Integer cvNumberExp) { this.cvNumberExp = cvNumberExp; }
+
+    public String getCvEducation() { return cvEducation; }
+    public void setCvEducation(String cvEducation) { this.cvEducation = cvEducation; }
+
+    public String getCvField() { return cvField; }
+    public void setCvField(String cvField) { this.cvField = cvField; }
+
+    public java.math.BigDecimal getCvCurrentSalary() { return cvCurrentSalary; }
+    public void setCvCurrentSalary(java.math.BigDecimal cvCurrentSalary) { this.cvCurrentSalary = cvCurrentSalary; }
+
+    public java.sql.Date getCvBirthday() { return cvBirthday; }
+    public void setCvBirthday(java.sql.Date cvBirthday) { this.cvBirthday = cvBirthday; }
+
+    public String getCvNationality() { return cvNationality; }
+    public void setCvNationality(String cvNationality) { this.cvNationality = cvNationality; }
+
+    public String getCvGender() { return cvGender; }
+    public void setCvGender(String cvGender) { this.cvGender = cvGender; }
+
+    public String getCvFileData() { return cvFileData; }
+    public void setCvFileData(String cvFileData) { this.cvFileData = cvFileData; }
 
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
--- SWP391/src/java/model/JobPostModel.java
+++ SWP391/src/java/model/JobPostModel.java
@@ -1,21 +1,35 @@
 package model;
 
+import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 public class JobPostModel {
     private int jobPostId;
     private String title;
+    private String description;
     private String category;
     private String position;
     private String location;
-    private Date deadline;
+    private BigDecimal offerMin;
+    private BigDecimal offerMax;
+    private Integer numberExp;
     private boolean visible;
+    private String typeJob;
+    private Date deadline;
+    private Timestamp dayCreate;
+    private Integer createdBy;
+    private String createdByName;
+    private int applyCount;
 
     public int getJobPostId() { return jobPostId; }
     public void setJobPostId(int jobPostId) { this.jobPostId = jobPostId; }
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
     public String getCategory() { return category; }
     public void setCategory(String category) { this.category = category; }
@@ -26,9 +40,33 @@ public class JobPostModel {
     public String getLocation() { return location; }
     public void setLocation(String location) { this.location = location; }
 
-    public Date getDeadline() { return deadline; }
-    public void setDeadline(Date deadline) { this.deadline = deadline; }
+    public BigDecimal getOfferMin() { return offerMin; }
+    public void setOfferMin(BigDecimal offerMin) { this.offerMin = offerMin; }
+
+    public BigDecimal getOfferMax() { return offerMax; }
+    public void setOfferMax(BigDecimal offerMax) { this.offerMax = offerMax; }
+
+    public Integer getNumberExp() { return numberExp; }
+    public void setNumberExp(Integer numberExp) { this.numberExp = numberExp; }
 
     public boolean isVisible() { return visible; }
     public void setVisible(boolean visible) { this.visible = visible; }
+
+    public String getTypeJob() { return typeJob; }
+    public void setTypeJob(String typeJob) { this.typeJob = typeJob; }
+
+    public Date getDeadline() { return deadline; }
+    public void setDeadline(Date deadline) { this.deadline = deadline; }
+
+    public Timestamp getDayCreate() { return dayCreate; }
+    public void setDayCreate(Timestamp dayCreate) { this.dayCreate = dayCreate; }
+
+    public Integer getCreatedBy() { return createdBy; }
+    public void setCreatedBy(Integer createdBy) { this.createdBy = createdBy; }
+
+    public String getCreatedByName() { return createdByName; }
+    public void setCreatedByName(String createdByName) { this.createdByName = createdByName; }
+
+    public int getApplyCount() { return applyCount; }
+    public void setApplyCount(int applyCount) { this.applyCount = applyCount; }
 }
